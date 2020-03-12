40b172c1d804a1eea140a00adc982aa726ffec0c
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2020-03-11 18:28:01 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int numero=0, anterior=1, resultado=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese La cantidad de numeros: ");
+        int n = sc.nextInt();
+        for(int i=1; i<n; i++){
+            resultado=numero+anterior;
+            numero=anterior;
+            anterior=resultado;
+            System.out.println(numero);
+        }
+    }
+}
